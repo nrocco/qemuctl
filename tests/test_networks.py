325b0ac8ff1992a2238a43aca691b478dc4063f2
Nico Di Rocco <dirocco.nico@gmail.com>
2022-11-08 00:04:21 +0100

    Fix tests for dnsmasq

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -4,8 +4,8 @@ from qemu.specs import NetworkSpec
 
 def test_config_with_basics():
     spec = NetworkSpec({"name": "br0", "logging": False})
-    config = generate_dnsmasq_config(spec)
-    assert "pid-file=pidfile" in config
+    config = generate_dnsmasq_config(spec, "/tmp")
+    assert "pid-file=/tmp/pidfile" in config
     assert "interface=br0" in config
     assert "except-interface=lo" in config
     assert "bind-interfaces" in config
@@ -15,14 +15,14 @@ def test_config_with_basics():
 
 def test_config_with_logging():
     spec = NetworkSpec({"name": "br0"})
-    config = generate_dnsmasq_config(spec)
+    config = generate_dnsmasq_config(spec, "/tmp")
     assert "log-queries" in config
-    assert "log-facility=./dnsmasq.log" in config
+    assert "log-facility=/tmp/dnsmasq.log" in config
 
 
 def test_config_with_dns():
     spec = NetworkSpec({"name": "br0", "dns": True})
-    config = generate_dnsmasq_config(spec)
+    config = generate_dnsmasq_config(spec, "/tmp")
     assert "strict-order" in config
     assert "domain-needed" in config
     assert "bogus-priv" in config
@@ -32,10 +32,10 @@ def test_config_with_dns():
 
 def test_config_with_dhcp():
     spec = NetworkSpec({"name": "br0", "ip_range": "127.0.0.0/24"})
-    config = generate_dnsmasq_config(spec)
+    config = generate_dnsmasq_config(spec, "/tmp")
     assert "dhcp-range=127.0.0.2,127.0.0.254,255.255.255.0" in config
     assert "dhcp-no-override" in config
     assert "dhcp-authoritative" in config
     assert "dhcp-lease-max=253" in config
-    assert "dhcp-hostsfile=hostsfile" in config
-    assert "dhcp-leasefile=leases" in config
+    assert "dhcp-hostsfile=/tmp/hostsfile" in config
+    assert "dhcp-leasefile=/tmp/leases" in config
